ccfbad3941ac07878229ea0f0f4e839687f6be81
WhatIsRove <whatisbuff@gmail.com>
2024-09-26 21:33:49 +0100

    Completed Tasky Lab 1

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
+import Task from './components/Task';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Tasky</h1>
+      <Task title="Dishes" deadline="Today" description="Wash the dishes.">
+        
+      </Task>
+      <Task title="Tidy" deadline="Tomorrow" description="Tidy rubbish and clean room.">
+        
+      </Task>
+      <Task title="Laundry" deadline="The Day After" description="Fold the laundry.">
+        
+      </Task>
     </div>
   );
 }
