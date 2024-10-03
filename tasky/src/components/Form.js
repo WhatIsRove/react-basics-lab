70e98e906117507a9e629887b3aa28c0bc085201
WhatIsRove <whatisbuff@gmail.com>
2024-10-03 10:56:06 +0100

    Add task functionality

--- tasky/src/components/Form.js
+++ tasky/src/components/Form.js
@@ -3,23 +3,23 @@ import React from 'react';
 const AddTaskForm = (props) => {
     return (
         <div>
-            <form>
+            <form onSubmit={props.submit}>
                 <label>
                     Task title:
                     <br />
-                    <input type="text" name="title" required />
+                    <input type="text" name="title" onChange={(event) => props.change(event)} required />
                 </label>
                 <br />
                 <label>
                     Due date:
                     <br />
-                    <input type="date" name="deadline" required />
+                    <input type="date" name="deadline" onChange={(event) => props.change(event)} required />
                 </label>
                 <br />
                 <label>
                     Details:
                     <br />
-                    <input type="text" name="description" required />
+                    <input type="text" name="description" onChange={(event) => props.change(event)} required />
                 </label>
                 <br />
                 <input type="submit" value="Submit" />
